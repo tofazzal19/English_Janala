2d4496c1fcee16b6970f2e94417783b525473c91
tofazzal19 <131471407+tofazzal19@users.noreply.github.com>
2025-09-10 22:52:22 +0600

    version2

--- script/index.js
+++ script/index.js
@@ -11,9 +11,42 @@ const loadLevelWord=(id)=> {
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
+    if(words.length == 0){
+       wordContainer.innerHTML =`
+       
+  <div class="text-center  col-span-full rounded-xl py-10 space-y-6 font-bangla">
+    <img class="mx-auto" src="./images/alert-error.png"/>
+  <p class="text-xl font-medium text-gray-500 py-10 space-y-6">এই এখনো কোন
+     Lesson যুক্ত করা হয়নি
+       </p>
+    <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান
+     </h2>
+  </div>`
+    }
+//     {
+//     "id": 50,
+//     "level": 5,
+//     "word": "Zephyr",
+//     "meaning": "মৃদু বাতাস / হালকা হাওয়া",
+//     "pronunciation": "জেফার"
+// }
 
     words.forEach((word) => {
-        console.log(word);
+        const card = document.createElement('div');
+        card.innerHTML =`
+        <div class="bg-white rounded-xl drop-shadow-sm text-center py-10 px-5 space-y-4">
+      <h2 class="font-bold text-2xl">${word.word ? word : 'শব্দ পাওয়া যায়নি'}</h2>
+      <p class="font-semibold">Meaning /Pronounciation</p>
+
+      <div class="font-medium text-2xl font-bangla ">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} /
+       ${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায়নি'}"</div>  
+      <div class="flex justify-between items-center">
+      <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+       <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+       </div>
+    </div>`;
+    wordContainer.append(card);
+         
     });
 }
 
